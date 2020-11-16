e1d7b9c80c418e0a444d60249953eff5f18b7df2
pedropaulo42 <pedroppaulo42@gmail.com>
2020-11-15 22:46:45 -0300

    Service layer and exception handling: findById/delete

---
+++ backend/src/main/java/com/backend/be/services/ProductService.java
@@ -0,0 +1,57 @@
+package com.backend.be.services;
+
+import java.util.List;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.backend.be.entities.Product;
+import com.backend.be.repositories.ProductRepository;
+import com.backend.be.services.exceptions.DatabaseException;
+import com.backend.be.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product insert(Product product) {
+		return repository.save(product);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public Product update(Long id, Product product) {
+		try {
+			Product entity = repository.getOne(id);
+			updateData(entity, product);
+			return repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	
+	private void updateData(Product entity, Product product) {
+		entity.setName(product.getName());
+		entity.setPrice(product.getPrice());
+		entity.setQuantity(product.getQuantity());
+		entity.setDescription(product.getDescription());
+	}
+}
